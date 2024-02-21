48e401526e1b456d388bfea782bf626d533d8621
Rodrigo Almeida <82787131+II2DII@users.noreply.github.com>
2024-02-21 15:45:05 -0300

    Alterações

--- app.py
+++ app.py
@@ -1,60 +1,57 @@
-from flask import Flask, render_template, request, response
+from flask import Flask, render_template, request, Response
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
 from time import sleep
 from helpers import *
+from selecionar_persona import *
+from selecionar_documentos import *
+from assistente_ecomart import *
 
 load_dotenv()
 
-cliente = OpenAI(API_KEY=os.getenv("OPENAI_API_KEY"))
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 modelo = "gpt-3.5-turbo"
 
 app = Flask(__name__)
 app.secret_key = 'alura'
 
-contexto = carrega("dados/ecomart.txt")
+assistente = criar_assistente()
+thread = criar_thread()
 
 def bot(prompt):
-  maximo_tentativas = 1
-  repeticao = 0
-  
-  while True:
-    try:
-            prompt_do_sistema = f"""
-            Você é um chatbot de atendimento a clientes de um e-commerce.
-            Você não deve responder perguntas que não sejam dados do ecommerce informado!
-            
-            Você deve gerar respostas utilizando o contexto abaixo
-            
-            #contexto:
-            {contexto}
-                """
-                
-            response = cliente.chat.completions.create(
-                messages=[
-                            {
-                                "role": "system",
-                                "content": prompt_do_sistema
-                            },
-                            {
-                                "role": "user",
-                                "content": prompt
-                            }
-                ],
-                temperature=1,
-                max_tokens=256,
-                top_p=1,
-                frequency_penalty=0,
-                presence_penalty=0,
-                modal = modelo)
-            return response
-    except Exception as erro:
-                repeticao += 1
-                if repeticao >= maximo_tentativas:
-                    return "Erro no GPT: %s" % erro
-                print('Erro de comunicação com OpenAI: ', erro)
-                sleep(1)
+    maximo_tentativas = 1
+    repeticao = 0
+
+    while True:
+        try:
+            cliente.beta.threads.messages.create(
+                    thread_id=thread.id,
+                    role = "user",
+                    content =  prompt
+                )
+
+            run = cliente.beta.threads.runs.create(
+                thread_id=thread.id,
+                assistant_id=assistente.id
+            )
+
+            while run.status !="completed":
+                run = cliente.beta.threads.runs.retrieve(
+                    thread_id=thread.id,
+                    run_id=run.id
+            )
+
+            historico = list(cliente.beta.threads.messages.list(thread_id=thread.id).data)
+            resposta = historico[0]
+            return resposta
+
+        except Exception as erro:
+                    repeticao += 1
+                    if repeticao >= maximo_tentativas:
+                        return "Erro no GPT: %s" % erro
+                    print('Erro de comunicação com OpenAI: ', erro)
+                    sleep(1)
 
 
 
@@ -62,7 +59,7 @@ def bot(prompt):
 def chat():
     prompt = request.json["msg"]
     resposta = bot(prompt)
-    texto_resposta = resposta.choices[0].message.content
+    texto_resposta = resposta.content[0].text.value
     return texto_resposta
 
 
---
+++ assistente_ecomart.py
@@ -0,0 +1,37 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+from time import sleep
+from helpers import *
+from selecionar_persona import *
+
+load_dotenv()
+
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-3.5-turbo"
+contexto = carrega("dados/ecomart.txt")
+
+def criar_thread():
+    return client.beta.threads.create()
+
+def criar_assistente():
+    assistente = cliente.beta.assistants.create(
+        name="Atendente EcoMart",
+        instructions = f"""
+            Você é um chatbot de atendimento a clientes de um e-commerce. 
+            Você não deve responder perguntas que não sejam dados do ecommerce informado!
+            Além disso, adote a persona abaixo para responder ao cliente.
+            
+            ## Contexto
+            {contexto}
+            
+            ## Persona
+            
+            {personas["neutro"]}
+        """,
+        model = modelo,
+    )
+    return assistente
+
+
+
